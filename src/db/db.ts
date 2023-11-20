4f514b057ccd88a0af5a09820f5a35cd69527a07
ElianArism <elianarism@gmail.com>
2023-11-20 06:22:46 -0300

    123

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,10 +3,10 @@ import type {Connection} from 'mysql2/typings/mysql/lib/Connection';
 
 export class MySqlDB {
   private static _connection: Connection = mysql.createConnection({
-    user: process.env.user,
-    password: process.env.password,
-    host: process.env.host,
-    database: process.env.database,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
   });
 
   constructor() {}
